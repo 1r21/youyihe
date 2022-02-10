de52fb29a5976e782fa02038a8a6e1aa91207f1c
heng1025 <472263356@qq.com>
2022-02-10 14:25:55 +0800

    chore: update release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,14 +1,23 @@
 import { execSync } from "child_process";
-import { $ } from "zx";
+import { $, chalk, fs } from "zx";
 
+// update version
 execSync("npx bumpp package.json packages/*/package.json", {
   stdio: "inherit",
 });
 
 const { version } = await fs.readJSON("package.json");
 
+// push to GitHub
 await $`git add .`;
 await $`git commit -m "chore: release v${version}"`;
 await $`git tag v${version}`;
 await $`git push`;
 await $`git push origin --tags`;
+
+// build
+await $`npm run build`;
+
+// publish
+await $`npx pnpm -r publish --access public --no-git-checks`;
+console.log(chalk.green(`Successfully published ${version}`));
